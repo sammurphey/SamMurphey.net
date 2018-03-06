d3742b7c20b50136fa57a40ed7bcb600e4b9ef76
Sam Murphey <weirdoonthebus@gmail.com>
2018-03-06 09:53:30 -0800

    Continued optimizing sass

--- src/js/Header.js
+++ src/js/Header.js
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 class LogoIcon extends Component {
 	render () {
 		return (
-			<Link to={`/`} id="logo_btn" className="header_btn">
-				<svg id="logo_icon" fill="#000" width="48" height="48" viewBox="0 0 100 100">
+			<Link to={`/`} id="logo_btn" className="btn">
+				<svg className="lrg icon" fill="#000" width="48" height="48" viewBox="0 0 100 100">
 					<polygon points="0 0,100 0,100 100,96 100,96 56,52 100,0 100" />
 					<g fill="#F9F9F9">
 						<polygon points="7 4,50 48,93 4" />
@@ -43,8 +43,8 @@ class MainNav extends Component {
 class SearchIcon extends Component {
 	render () {
 		return (
-			<Link to={`/search`} id="search_btn" className="header_btn">
-				<svg id="search_icon" width="48" height="48" viewBox="0 0 100 100">
+			<Link to={`/search`} id="search_btn" className="btn">
+				<svg className="sml icon" width="48" height="48" viewBox="0 0 100 100">
 					<polygon id="search_icon_shape_1" fill="none" stroke="#000" strokeWidth="12" transform="rotate(45) translate(24 -36) scale(0.7)" points="5 50,30 10,70 10,95 50,70 90,30 90" />
 					<rect id="search_icon_shape_2" fill="#000" transform="rotate(315) translate(-4 84)" height="48" width="12" />
 				</svg>
