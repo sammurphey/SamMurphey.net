15f18831bdda94d15b9545791fe6b24677c1c497
Sam Murphey <weirdoonthebus@gmail.com>
2018-10-27 14:34:32 -0700

    Updated links in support panel

--- src/components/Support.php
+++ src/components/Support.php
@@ -5,15 +5,15 @@
 	<nav>
 		<ul>
 	<?php
-		echo "<li><a href='https://patreon.com/sammurphey' class='btn cta_btn'>";
+		echo "<li><a href='https://patreon.com/sammurphey' rel='noopener noreferrer' class='btn cta_btn' target='_blank'>";
 		$btn_img = file_get_contents($cdn_root . "ui/social/patreon.svg");
 		echo $btn_img;
 		echo "Patreon</a></li>";
-		echo "<li><a href='https://www.paypal.me/sammurphey' class='btn cta_btn'>";
+		echo "<li><a href='https://www.paypal.me/sammurphey' rel='noopener noreferrer' class='btn cta_btn' target='_blank'>";
 		$btn_img = file_get_contents($cdn_root . "ui/social/paypal.svg");
 		echo $btn_img;
 		echo "PayPal</a></li>";
-		echo "<li><a href='http://ko-fi.com/sammurphey' class='btn cta_btn'>";
+		echo "<li><a href='http://ko-fi.com/sammurphey' rel='noopener noreferrer' class='btn cta_btn' target='_blank'>";
 		$btn_img = file_get_contents($cdn_root . "ui/social/kofi.svg");
 		echo $btn_img;
 		echo "KoFi</a></li>";
